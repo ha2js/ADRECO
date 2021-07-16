9a67930a5298c6503d2ed0ac45f748911a5563e0
ss-hoon <lshoon1128@naver.com>
2021-07-16 15:27:22 +0900

    기존 광고판과 템플릿 프로젝트 merge

--- source/adreco-front/src/App.vue
+++ source/adreco-front/src/App.vue
@@ -1,22 +1,28 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <notifications></notifications>
+    <router-view :key="$route.fullPath"></router-view>
   </div>
 </template>
 
 <script>
-
-export default {
-
-}
+  export default {
+    methods: {
+      disableRTL() {
+        if (!this.$rtl.isRTL) {
+          this.$rtl.disableRTL();
+        }
+      },
+      toggleNavOpen() {
+        let root = document.getElementsByTagName('html')[0];
+        root.classList.toggle('nav-open');
+      }
+    },
+    mounted() {
+      this.$watch('$route', this.disableRTL, { immediate: true });
+      this.$watch('$sidebar.showSidebar', this.toggleNavOpen)
+    }
+  };
 </script>
 
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-</style>
+<style lang="scss"></style>
