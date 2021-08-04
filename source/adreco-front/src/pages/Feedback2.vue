9e8ddbaa1c5d30b29980a49c36d14583312f34a7
Jong Seo Ha <hajongse@naver.com>
2021-08-04 22:50:11 +0900

    광고판 시청률 UI 수정 중

--- source/adreco-front/src/pages/Feedback2.vue
+++ source/adreco-front/src/pages/Feedback2.vue
@@ -1,31 +1,49 @@
 <template>
-  <div>
-    <!--
-      <div class="col-lg-4" :class="{'text-right': isRTL}" >
-        <card type="chart">
+<div>
+  <div class="fb2Select">
+    <select id="category-option" v-model="category">
+      <option disabled value="">선택하세요</option>
+      <option>test1</option>
+      <option>test2</option>
+      <option>test3</option>
+    </select>
+    {{category}}
+
+    <select id="product-option" v-model="product">
+      <option disabled value="">선택하세요</option>
+      <option>test1</option>
+      <option>test2</option>
+      <option>test3</option>
+    </select>
+    {{product}}
+
+  </div>
+  <div class="fb2Chart">
+      <div id="chartFloat" class="col-lg-4" :class="{'text-right': isRTL}"  >
+        <card id="cardWidth" type="chart">
           <template slot="header">
-            <h5 class="card-category">{{$t('dashboard.completedTasks')}}광고별 시청률</h5>
+            <h5 class="card-category">Viewer Ratings By Advertisement</h5>
             <h3 class="card-title"><i class="tim-icons icon-single-02"></i> 남성</h3>
           </template>
-          <div class="chart-area">
+          <div class="chart-area" >
             <line-chart style="height: 100%"
-                        chart-id="green-line-chart"
-                        :chart-data="greenLineChart.chartData"
-                        :gradient-stops="greenLineChart.gradientStops"
-                        :extra-options="greenLineChart.extraOptions">
+                        chart-id="blue-line-chart"
+                        :chart-data="blueLineChart.chartData"
+                        :gradient-stops="blueLineChart.gradientStops"
+                        :extra-options="blueLineChart.extraOptions">
             </line-chart>
           </div>
         </card>
       </div> 
-      -->
-      <div class="col-lg-4" :class="{'text-right': isRTL}">
-        <card type="chart">
+
+      <div id="chartFloat" class="col-lg-4" :class="{'text-right': isRTL}" style="margin-left:00px;">
+        <card id="cardWidth" type="chart">
           <template slot="header">
-            <h5 class="card-category"><!--{{$t('dashboard.completedTasks')}}-->광고별 시청률</h5>
+            <h5 class="card-category">Viewer Ratings By Advertisement</h5>
             <h3 class="card-title"><i class="tim-icons icon-single-02"></i> 여성</h3>
           </template>
           <div class="chart-area">
-            <line-chart style="height: 100%"
+            <line-chart style="height: 100%;"
                         chart-id="red-line-chart"
                         :chart-data="redLineChart.chartData"
                         :gradient-stops="redLineChart.gradientStops"
@@ -35,6 +53,7 @@
         </card>
       </div>
   </div>
+</div>
 </template>
 <script>
   import LineChart from '@/components/Charts/LineChart';
@@ -53,6 +72,28 @@
     },
     data() {
       return {
+        blueLineChart: {
+          extraOptions: chartConfigs.greenChartOptions,
+          chartData: {
+            labels: ['10대', '20대', '30대', '40대', '50대','60대'],
+            datasets: [{
+              label: "제품 관심도",
+              fill: true,
+              borderColor: config.colors.info,
+              borderWidth: 2,
+              borderDash: [],
+              borderDashOffset: 0.0,
+              pointBackgroundColor: config.colors.info,
+              pointBorderColor: 'rgba(255,255,255,0)',
+              pointHoverBackgroundColor: config.colors.info,
+              pointBorderWidth: 20,
+              pointHoverRadius: 4,
+              pointHoverBorderWidth: 15,
+              pointRadius: 4,
+              data: [90, 50, 83, 40, 30,10],
+            }]
+          }
+        },
         redLineChart: {
           extraOptions: chartConfigs.greenChartOptions,
           chartData: {
@@ -73,37 +114,39 @@
               pointRadius: 4,
               data: [80, 90, 77, 40, 10,30],
             }]
-          },
-          gradientColors: ['rgba(66,134,121,0.15)', 'rgba(66,134,121,0.0)', 'rgba(66,134,121,0)'],
-          gradientStops: [1, 0.4, 0],
+          }
         },
-        blueLineChart: {
-          extraOptions: chartConfigs.greenChartOptions,
-          chartData: {
-            labels: ['10대', '20대', '30대', '40대', '50대','60대'],
-            datasets: [{
-              label: "제품 관심도",
-              fill: true,
-              borderColor: config.colors.danger,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              pointBackgroundColor: config.colors.danger,
-              pointBorderColor: 'rgba(255,255,255,0)',
-              pointHoverBackgroundColor: config.colors.danger,
-              pointBorderWidth: 20,
-              pointHoverRadius: 4,
-              pointHoverBorderWidth: 15,
-              pointRadius: 4,
-              data: [80, 90, 77, 40, 10,30],
-            }]
-          },
-          gradientColors: ['rgba(66,134,121,0.15)', 'rgba(66,134,121,0.0)', 'rgba(66,134,121,0)'],
-          gradientStops: [1, 0.4, 0],
-        }
+        category: '',
+        product: '',
       }
-    },
+    }
   };
 </script>
-<style>
+<style lang="scss" scoped>
+  .fb2Select {
+    text-align:center;
+    padding: 15px 0 90px 0;
+  }
+  #category-option, #product-option {
+    background-color:rgb(39,41,61);
+    color:rgb(255,255,255);
+    border: 1px solid rgb(39,41,61);
+  }
+  #category-option {
+    width:15%;
+    height:50px;
+  }
+  #product-option {
+    width:50%;
+    height:50px;
+  }
+  .fb2Chart {
+    margin:0 auto;
+    position:absolute;
+    width:100%;
+    left:25%;
+  } 
+  #chartFloat {
+    float:left;
+  }
 </style>
